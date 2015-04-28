f0d981de20eeb14feb79f0b4217b45afdf5506dd
garretthogan <garretthogan18@gmail.com>
2015-04-28 16:01:58 -0500

    detyu
    dy

---
+++ Assets/Scripts/Garrett's Scripts/CGUIManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CGUIManager : MonoBehaviour {
+
+    private static CGUIManager instance;
+    private CGUIManager() { }
+
+    public static CGUIManager Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = new CGUIManager();
+            }
+            return instance;
+        }
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
